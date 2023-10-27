c99b6705d731fcbaace3955a969278f7c1fc8881
Scott Harper <orcein@gmail.com>
2023-10-27 19:33:26 +0900

    api endpoint and list of blog posts

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,5 +1,5 @@
 export async function load({ params }) {
-    const post = await import(`../../../blog/${params.slug}.md`);
+    const post = await import(`/src/blog/${params.slug}.md`);
     const { title, date } = post.metadata;
     const content = post.default;
 
