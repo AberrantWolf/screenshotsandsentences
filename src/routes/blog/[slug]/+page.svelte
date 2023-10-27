e50107b830b5446e8a3d1285e6c63ea1c01e0857
Scott Harper <orcein@gmail.com>
2023-10-27 22:24:57 +0900

    Added a navbar thing

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Toc from "svelte-toc";
+
     export let data;
     const { title, date, Content } = data;
 </script>
@@ -7,6 +9,8 @@
     <title>Blog = {title}</title>
     <meta property="og:title" content={title} />
 </svelte:head>
+
+<Toc>--pageBody="article"</Toc>
 
 <article>
     <h1>{title}</h1>
